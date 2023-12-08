92a890969fcf68271b10479312719f12213231bb
joaoruss0 <r0u6s12s18o@gmail.com>
2023-12-08 01:27:31 +0000

    Add quicksort

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day2part1;
 mod day2part2;
+mod day4part1;
 
 use std::{
     fs::File,
@@ -17,4 +18,38 @@ fn read_file(file_name: &str) -> Vec<String> {
     let buf_reader = BufReader::new(file);
     return buf_reader.lines().map(|l| l.expect("can not parse line"))
         .collect();
+}
+
+fn quick_sort(numbers: &mut Vec<u8>, start: usize, end: isize) {
+
+    if end <= start as isize {
+        return;
+    }
+
+    let mut i: usize = end as usize;
+    let mut pivot = end.clone() as usize;
+
+    while i > start {
+
+        if numbers[i - 1] > numbers[pivot] {
+
+            move_to_new_position(numbers, i - 1, pivot);
+            pivot -=1;
+        }
+        i-=1;
+    }
+
+    quick_sort(numbers, start, pivot as isize - 1);
+    quick_sort(numbers, pivot + 1, end.clone());
+}
+
+fn move_to_new_position(numbers: &mut Vec<u8>, mut index: usize, goal: usize) {
+
+    while index < goal {
+
+        let stored_number = numbers[index];
+        numbers[index] = numbers[index +1];
+        numbers[index +1] = stored_number;
+        index +=1;
+    }
 }
